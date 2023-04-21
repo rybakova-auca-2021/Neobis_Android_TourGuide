7bc8fc755fb0bce8f51556218ae8dd4f191f4d75
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-04-22 00:28:42 +0600

    Added cinema and fitness center fragments

--- app/src/main/java/com/example/tourguide/CinemaFragment.kt
+++ app/src/main/java/com/example/tourguide/CinemaFragment.kt
@@ -5,55 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tourguide.databinding.FragmentCinemasBinding
+import com.example.tourguide.databinding.FragmentFitnessCenterBinding
+import com.example.tourguide.databinding.FragmentRestuarantsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CinemaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CinemaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentCinemasBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cinemas, container, false)
+    ): View {
+        binding = FragmentCinemasBinding.inflate(inflater, container, false)
+        binding.recyclerViewCinemas.layoutManager = LinearLayoutManager(requireContext())
+        val list = arrayListOf(
+            Place(R.drawable.tsum, getString(R.string.tsum_title), getString(R.string.tsum_address), getString(
+                R.string.tsum_time), getString(R.string.tsum_distance), getString(R.string.tsum_price),
+                getString(R.string.tsum_description)),
+            Place(R.drawable.broadway, getString(R.string.broadway_title), getString(R.string.broadway_address), getString(
+                R.string.broadway_time), getString(R.string.broadway_distance), getString(R.string.broadway_price),
+                getString(R.string.broadway_description))
+        )
+        binding.recyclerViewCinemas.adapter = Adapter(list)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment cinemas.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CinemaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
